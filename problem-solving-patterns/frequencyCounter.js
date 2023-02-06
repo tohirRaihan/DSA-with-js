f48ae1e27a936c3c88f5c2213f3dfb70f346b16f
Tohir Raihan <tohir.raihan@gmail.com>
2023-02-07 01:54:47 +0600

    added anagram problem

--- problem-solving-patterns/frequencyCounter.js
+++ problem-solving-patterns/frequencyCounter.js
@@ -16,15 +16,42 @@ function same(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   const frequencyCounter1 = {};
   const frequencyCounter2 = {};
-  arr1.forEach((val) => {
-    frequencyCounter1[val] = ++frequencyCounter1[val] || 1;
-  });
-  arr2.forEach((val) => {
-    frequencyCounter2[val] = ++frequencyCounter2[val] || 1;
-  });
+
+  arr1.forEach(
+    (val) => (frequencyCounter1[val] = ++frequencyCounter1[val] || 1)
+  );
+  arr2.forEach(
+    (val) => (frequencyCounter2[val] = ++frequencyCounter2[val] || 1)
+  );
+
   for (const key in frequencyCounter1) {
     if (!(key ** 2 in frequencyCounter2)) return false;
     if (frequencyCounter1[key] !== frequencyCounter2[key ** 2]) return false;
   }
+  return true;
+}
+
+/**
+ * PROBLEM #2:
+ * Given two strings, write a function to determine if the second string is
+ * an anagram of the first. An anagram is a word, name or phrase formed by
+ * rearranging the letters of another, such as cinema, formed from from iceman.
+ *
+ * EXAMPLES:
+ * validAnagram('', '') --> true
+ * validAnagram('aaz', 'zza') --> false
+ * validAnagram('anagram', 'nagaram') --> true
+ * validAnagram('', '') --> true
+ */
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  const lookup = {};
+  for (const letter of str1) lookup[letter] = ++lookup[letter] || 1;
+  for (const letter of str2) {
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) return false;
+    else lookup[letter]--;
+  }
+
   return true;
 }
