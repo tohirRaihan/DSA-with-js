054b5d94948c5cac66610b8ba1cdb70d1b675ae6
Tohir Raihan <tohir.raihan@gmail.com>
2023-02-07 20:36:02 +0600

    count unique value function added

---
+++ problem-solving-patterns/slidingWindow.js
@@ -0,0 +1,19 @@
+//--> This pattern involves creating a window which can either be an array or number from one position to another.
+//--> Very useful for keeping track of a subset of data in an array/string etc.
+
+/**
+ * PROBLEM #1:
+ * Write a function called maxSubarraySum which accepts an array of integers 
+ * and a number called n. The function should calculate the maximum sum of n 
+ * consecutive elements in the array.
+ *
+ * EXAMPLES:
+ * maxSubarraySum([1,2,5,2,8,1,5],2) --> 10
+ * maxSubarraySum([1,2,5,2,8,1,5],4) --> 17
+ * maxSubarraySum([4,2,1,6],1) --> 6
+ * maxSubarraySum([4,2,1,6,2],4) --> 13
+ * maxSubarraySum([],4) --> null
+ */
+function maxSubarraySum(arr, n) {
+  
+}
