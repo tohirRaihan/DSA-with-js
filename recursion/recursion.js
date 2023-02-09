d3c51133fef0eb8369d22cc80f56c897b7ac4cd1
Tohir Raihan <tohir.raihan@gmail.com>
2023-02-09 20:30:24 +0600

    added some recursive functions

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -35,3 +35,35 @@ function productOfArray(arr) {
   if (arr.length === 0) return 1;
   return arr[0] * productOfArray(arr.slice(1));
 }
+
+function reverseStr(str) {
+  if (str.length <= 1) return str;
+  return reverseStr(str.slice(1)) + str[0];
+}
+
+// palindrome means same if we read the string backword
+function isPalindrome(str) {
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+
+  if (str[0] === str.at(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
+}
+
+function someRecursive(array, callback) {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1), callback);
+}
+
+function flatten(oldArr) {
+  var newArr = [];
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]));
+    } else {
+      newArr.push(oldArr[i]);
+    }
+  }
+  return newArr;
+}
