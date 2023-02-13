98863d09b197adc9c6eb770d56d8ba51238d8fdd
Tohir Raihan <tohir.raihan@gmail.com>
2023-02-13 15:57:16 +0600

    insertion sort added

--- algorithm/sorting/elementary-sorting/insertionSort.js
+++ algorithm/sorting/elementary-sorting/insertionSort.js
@@ -1,29 +1,15 @@
 //--> Builds up the sort by gradually creating a larger left half which is always sorted
 
 function insertionSort(arr) {
-  // loop from the index 1 of the array to the end
-  for(let i =1; i<arr.length; i++) {
-    let currentValue = arr[i];
-    let currPos = null;
-
-    for(j=i; j>=0; j--) {
-      if(arr[j-1] > arr[j]) {
-        arr[j] =arr[j-1];
-        currPos = j-1;
-      } else break;
+  var currentVal;
+  for (var i = 1; i < arr.length; i++) {
+    currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
     }
-    if(currPos !== null) arr[currPos] = currentValue;
+    arr[j + 1] = currentVal;
   }
-  console.log(arr)
-  // check if the value is smaller than the previous value
-    // if true than copy the previous value to the current
-    // else current position has current value
-
   return arr;
 }
 
-insertionSort([2,1,5,3,8,4,9])
-/*
-2 1 5 3 8 4 9
-2 2 5 3 8 4 9
- */
+insertionSort([2, 1, 9, 76, 4]);
