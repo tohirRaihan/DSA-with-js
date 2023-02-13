31774f889682715f416ff1a75514a57ee69f86f1
Tohir Raihan <tohir.raihan@gmail.com>
2023-02-13 16:55:41 +0600

    update insertion sort

--- algorithm/sorting/elementary-sorting/insertionSort.js
+++ algorithm/sorting/elementary-sorting/insertionSort.js
@@ -1,4 +1,6 @@
 //--> Builds up the sort by gradually creating a larger left half which is always sorted
+//--> O(n**2) -- it is good for nearly sorted array
+//--> It is also good if you get one data at a time and place that data in the array with sorted position (it is called online algorithm)
 
 function insertionSort(arr) {
   var currentVal;
@@ -13,3 +15,9 @@ function insertionSort(arr) {
 }
 
 insertionSort([2, 1, 9, 76, 4]);
+
+/** PSEUDOCODE **
+ --Start by picking the second element in the array
+ --Now compare the second element with the one before it and swap if necessary.
+ --Continue to the next element and if it is in the incorrect order, iterate through the sorted portion (i.e. the left side) to place the element in the correct place.
+**/
