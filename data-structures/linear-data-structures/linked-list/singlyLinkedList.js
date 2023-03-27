6b69779fca5ab04e101b00000cf3bbfd8befcc69
Tohir Raihan <tohir.raihan@gmail.com>
2023-03-27 15:43:32 +0600

    update singly linked list

--- data-structures/linear-data-structures/linked-list/singlyLinkedList.js
+++ data-structures/linear-data-structures/linked-list/singlyLinkedList.js
@@ -24,9 +24,62 @@ class SinglyLinkedList {
     this.tail = newNode;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    this.length++;
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+      return this;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
 }
 
 const first = new SinglyLinkedList();
-first.push(1).push(2).push(3).push(4);
+first.push(1).push(2).push(3).push(4).unshift(100);
 
-console.log(first.head.next);
+console.log(first);
